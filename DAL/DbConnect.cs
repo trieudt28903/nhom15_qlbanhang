ad66852dc2f399b905ed81326a018f9aeec905f8
trieudt28903 <trieu28903@gmail.com>
2023-10-25 22:12:49 +0700

    trieule

---
+++ DAL/DbConnect.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DbConnect
+    {
+        static string connstr = ConfigurationManager.ConnectionStrings["SalesManagement"].ToString();
+        protected SqlConnection _conn = new SqlConnection(connstr);
+    }
+}
